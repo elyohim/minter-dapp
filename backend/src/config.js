06d5f5243fcae0192917ce42c16bf80e63e7fec4
elyohim <102671515+elyohim@users.noreply.github.com>
2022-04-02 21:50:54 +0800

    presale

--- backend/src/config.js
+++ backend/src/config.js
@@ -63,7 +63,7 @@ const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2022-05-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-04-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-04-02T11:30:48+00:00"; // Optional. Eg: 2022-04-08T11:30:48+00:00
 const ROYALTY_SHARE = 500; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xd43881a39E053cE92B04e56B7BE63D821D4C352c"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
